6dbbd6d2ad75a5f0c99754cf1542eba2e58bf51b
Yang Li <jackli900421@hotmail.com>
2018-08-23 14:24:36 +0800

    留言版实装，并实现日期格式化

--- src/components/home.vue
+++ src/components/home.vue
@@ -11,147 +11,88 @@
         </div>
         <div class="row">
             <div class="col-lg-12">
-                <div class="tm-video-container">
-                    <video id="tm-welcome-video" class="tm-welcome-video" autoplay loop muted>
-                        <source :src="videoSrc" type="video/mp4"> Your browser does not support the video tag.
-                        </video>
-                        <div id="tm-video-loader" v-if="!videoSrc"></div>
-                        <div id="tm-video-text-overlay" class="tm-video-text-overlay d-none">
-                            <h1>
-                                <div id="rotate" class="tm-video-text">
-                                    <div>This is timeless</div>
-                                    <div>We are invincible</div>
-                                    <div>Quite unbeatable</div>
-                                    <div>and indestructible</div>
-                                </div>
-                            </h1>
+                <div class="tm-video-container" style="overflow:hidden;">
+                <video id="tm-welcome-video" class="tm-welcome-video" autoplay loop muted>
+                    <source :src="videoSrc" type="video/mp4"> Your browser does not support the video tag.
+                </video>
+                <div id="tm-video-loader" v-if="!videoSrc"></div>
+                <div id="tm-video-text-overlay" class="tm-video-text-overlay d-none">
+                    <h1>
+                        <div id="rotate" class="tm-video-text">
+                            <div>This is timeless</div>
+                            <div>We are invincible</div>
+                            <div>Quite unbeatable</div>
+                            <div>and indestructible</div>
                         </div>
-
-                    </div>
+                    </h1>
+                </div>
+                <!-- <img src="https://picsum.photos/g/1920/1080/?random" alt="" > -->
                 </div>
             </div>
         </div>
-        <div class="container tm-container-2">
-            <div class="row">
-                <div class="col-lg-12">
-                    <h2 class="tm-welcome-text">记录我项目和生活的近况</h2>
-                </div>
-            </div>
-            <div class="row tm-section-mb">
-                <div class="col-lg-12">
-                    <div class=" tm-timeline-item">
-                        <div class="tm-timeline-item-inner">
-                            <img src="https://picsum.photos/160/160/?gravity=north" alt="Image" class="rounded-circle tm-img-timeline">
-                            <div class="tm-timeline-connector">
-                                <p class="mb-0">&nbsp;</p>
-                            </div>
-                            <div class="tm-timeline-description-wrap">
-                                <div class="tm-bg-dark tm-timeline-description">
-                                    <div class="title-group">
-                                        <span class="tm-title tm-font-400">公告板项目上线</span><span class="tm-category">项目</span>
-                                    </div>
-                                    <p>公告板项目上线。用来记录我的近况，包含生活方面和项目方面。技术栈：前端VueJS,数据存储LeanCloud</p>
-                                    <p class="float-right mb-0 date-created">2018-07-29</p>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="tm-timeline-connector-vertical"></div>
-                    </div>
-
-                    <div class="tm-timeline-item">
-                        <div class="tm-timeline-item-inner">
-                            <img src="https://picsum.photos/160/160/?gravity=east" alt="Image" class="rounded-circle tm-img-timeline">
-                            <div class="tm-timeline-connector">
-                                <p class="mb-0">&nbsp;</p>
-                            </div>
-                            <div class="tm-timeline-description-wrap">
-                                <div class="tm-bg-dark tm-timeline-description">
-                                    <div class="title-group">
-                                        <span class="tm-title tm-font-400">公告板项目上线</span><span class="tm-category">项目</span>
-                                    </div>
-                                    <p>公告板项目上线。用来记录我的近况，包含生活方面和项目方面。技术栈：前端VueJS,数据存储LeanCloud</p>
-                                    <p class="float-right mb-0 date-created">2018-07-29</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="tm-timeline-connector-vertical"></div>
-                    </div>
-
-                    <div class="tm-timeline-item">
-                        <div class="tm-timeline-item-inner">
-                            <img src="https://picsum.photos/160/160/?gravity=south" alt="Image" class="rounded-circle tm-img-timeline">
-                            <div class="tm-timeline-connector">
-                                <p class="mb-0">&nbsp;</p>
-                            </div>
-                            <div class="tm-timeline-description-wrap">
-                                <div class="tm-bg-dark tm-timeline-description">
-                                    <div class="title-group">
-                                        <span class="tm-title tm-font-400">公告板项目上线</span><span class="tm-category">项目</span>
-                                    </div>
-                                    <p>公告板项目上线。用来记录我的近况，包含生活方面和项目方面。技术栈：前端VueJS,数据存储LeanCloud</p>
-                                    <p class="float-right mb-0 date-created">2018-07-29</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="tm-timeline-connector-vertical"></div>
-                    </div>
-
-                    <div class="tm-timeline-item">
-                        <div class="tm-timeline-item-inner">
-                            <img src="https://picsum.photos/160/160/?gravity=west" alt="Image" class="rounded-circle tm-img-timeline">
-                            <div class="tm-timeline-connector">
-                                <p class="mb-0">&nbsp;</p>
-                            </div>
-                            <div class="tm-timeline-description-wrap">
-                                <div class="tm-bg-dark tm-timeline-description">
-                                    <div class="title-group">
-                                        <span class="tm-title tm-font-400">公告板项目上线</span><span class="tm-category">项目</span>
-                                    </div>
-                                    <p>公告板项目上线。用来记录我的近况，包含生活方面和项目方面。技术栈：前端VueJS,数据存储LeanCloud</p>
-                                    <p class="float-right mb-0 date-created">2018-07-29</p>
-                                </div>
-                            </div>
-                        </div>
-
-                    </div>
-
-                </div>
-            </div>
-            <!--  row -->
-            <hr>
-               <div class="col-lg-12 mb-5" v-if="currentUser">
-                <h3 class="mb-4 tm-text-gray">{{userInfo.username}},发表近况 <a href="" @click="logout()">[注 销]</a></h3>
-               
-                    <div class="row">
-                        <div class="form-group col-xl-6">
-                            <input v-model="title" type="text" id="comment_title" name="comment_title" class="form-control" placeholder="标题" required/>
-                        </div>
-                        <div class="form-group col-xl-6">
-                            <input v-model="category" type="text" id="comment_category" name="comment_category" class="form-control" placeholder="分类" required/>
-                        </div>
-                    </div>
-                    <div class="form-group">
-                        <textarea v-model="comment" id="comment_message" name="comment_message" class="form-control" rows="6" placeholder="近况..." required></textarea>
-                    </div>
-                    <button class="btn tm-btn-send" @click="postComments()">发 表</button>
-                
-            </div>
-        </div>
-        <hr v-if="currentUser">
-        <!-- Footer -->
-        <footer class="row mt-5 mb-5">
-            <div class="col-lg-12">
-                <p class="text-center tm-text-gray tm-copyright-text mb-0">Copyright &copy;
-                    <span class="tm-current-year">2018</span> jackyangli.com 
-                    
-                    | Design: Timeless
-                </p>
-            </div>
-        </footer>
     </div>
-</template>
+    <div class="container tm-container-2">
+        <div class="row">
+            <div class="col-lg-12">
+                <h2 class="tm-welcome-text">记录我项目和生活的近况</h2>
+            </div>
+        </div>
+        <div class="row tm-section-mb">
+            <div class="col-lg-12">
+                <div class=" tm-timeline-item"  v-for="(microblog,key) in microblogs" :key="key">
+                    <div class="tm-timeline-item-inner">
+                        <img :src="profileSrc(key)" alt="Image" class="rounded-circle tm-img-timeline">
+                        <div class="tm-timeline-connector">
+                            <p class="mb-0">&nbsp;</p>
+                        </div>
+                        <div class="tm-timeline-description-wrap">
+                            <div class="tm-bg-dark tm-timeline-description">
+                                <div class="title-group">
+                                    <span class="tm-title tm-font-400">{{microblog.attributes.title}}</span><span class="tm-category">{{microblog.attributes.category}}</span>
+                                </div>
+                                <p>{{microblog.attributes.comment}}</p>
+                                <p class="float-right mb-0 date-created">{{microblog.createdAt.getFullYear()}}-{{microblog.createdAt.getMonth() + 1}}-{{microblog.createdAt.getUTCDate()}}</p>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- 最后一条碎碎念不显示下方连接条 -->
+                    <div class="tm-timeline-connector-vertical" v-if="key!==(microblogs.length - 1)"></div>
+                </div>
 
+            </div>
+        </div>
+        <!--  row -->
+        <hr>
+        <div class="col-lg-12 mb-5" v-if="currentUser">
+        <h3 class="mb-4 tm-text-gray">{{userInfo.username}},发表近况 <a href="" @click="logout()">[注 销]</a></h3>
+        
+            <div class="row">
+                <div class="form-group col-xl-6">
+                    <input v-model="title" type="text" id="comment_title" name="comment_title" class="form-control" placeholder="标题" required/>
+                </div>
+                <div class="form-group col-xl-6">
+                    <input v-model="category" type="text" id="comment_category" name="comment_category" class="form-control" placeholder="分类" required/>
+                </div>
+            </div>
+            <div class="form-group">
+                <textarea v-model="comment" id="comment_message" name="comment_message" class="form-control" rows="6" placeholder="近况..." required></textarea>
+            </div>
+            <button class="btn tm-btn-send" @click="postComments()">发 表</button>
+        </div>
+    </div>
+    <hr v-if="currentUser">
+    <!-- Footer -->
+    <footer class="row mt-5 mb-5">
+        <div class="col-lg-12">
+            <p class="text-center tm-text-gray tm-copyright-text mb-0">Copyright &copy;
+                <span class="tm-current-year">2018</span> jackyangli.com 
+                
+                | Design: Timeless
+            </p>
+        </div>
+    </footer>
+  </div>
+</template>
 <script>
 import videoSource from "@/assets/video.mp4";
 export default {
@@ -161,8 +102,16 @@ export default {
       videoSrc: videoSource,
       title: "",
       category: "",
-      comment: null
+      comment: null,
+      microblogs: null
     };
+  },
+  computed: {
+    profileSrc() {
+      return function(index) {
+        return "https://picsum.photos/160/160/?random=v" + index;
+      };
+    }
   },
   methods: {
     logout() {
@@ -179,7 +128,7 @@ export default {
       microBlog.set("comment", this.comment);
       microBlog.save().then(
         function(comment) {
-          console.log("objectId is " + comment.id);
+          alert("发布成功!id是 " + comment.id);
         },
         function(error) {
           console.error(error);
@@ -196,15 +145,16 @@ export default {
         username: this.currentUser.get("username")
       };
     } else {
-      console.log("当前未登录，正在跳转登录页面");
+      console.log("当前为游客界面");
       //   this.$router.push('/login')
     }
   },
   mounted() {
-    var query = new AV.Query("MicroBlog");
+    let _this = this;
+    let query = new AV.Query("MicroBlog");
     query.descending("createdAt");
-    query.find().then(function(comment) {
-      console.log(comment);
+    query.find().then(function(microblogs) {
+      _this.microblogs = microblogs;
     });
     // query.find().then(function (lists) {
     //                 // 遍历查找结果
